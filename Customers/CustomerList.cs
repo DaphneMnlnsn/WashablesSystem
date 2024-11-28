e1cabaf5586f81a4850504d6f91e631ae0e27bef
kianaMartin <martinkiana45@gmail.com>
2024-11-28 22:26:44 +0800

    allow edit restore archived and delete in customers

--- Customers/CustomerList.cs
+++ Customers/CustomerList.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WashablesSystem.Classes;
 
 namespace WashablesSystem
 {
@@ -30,31 +31,45 @@ namespace WashablesSystem
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            if(what.Text.Equals("edit"))
+            if(what.Text.Equals("Edit"))
             {
-                EditCustomer editCustomer = new EditCustomer();
+                EditCustomer editCustomer = new EditCustomer(custNo.Text);
                 editCustomer.ShowDialog();
             }
             else
             {
-                DialogResult dialogResult = MessageBox.Show("Restore this information?", "Confirm Restore", MessageBoxButtons.YesNo);
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to restore this customer information?\nThis will be moved to customer information.", "Confirm Delete", MessageBoxButtons.YesNo);
                 if (dialogResult == DialogResult.Yes)
                 {
-                    //call restore method here
+                    CustomerClass customerClass = new CustomerClass();
+                    customerClass.restoreCustomer(custNo.Text);
                 }
+
             }
             
         }
 
         private void btnTrash_Click(object sender, EventArgs e)
         {
-            if (what.Text.Equals("edit"))
+            if (what.Text.Equals("Edit"))
             {
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete this customer information?\nThis will be moved to the user archive.", "Confirm Delete", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    CustomerClass customerClass = new CustomerClass();
+                    customerClass.archiveCustomer(custNo.Text);
+                }
                 //call archive method here
             }
             else
             {
                 //call delete method here
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete this customer information?\nThis will be deleted permanently.", "Confirm Delete", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    CustomerClass customerClass = new CustomerClass();
+                    customerClass.deleteCustomer(custNo.Text);
+                }
             }
         }
     }
--- Customers/EditCustomer.cs
+++ Customers/EditCustomer.cs
@@ -7,15 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WashablesSystem.Classes;
 
 namespace WashablesSystem
 {
+   
     public partial class EditCustomer : Form
     {
-        public EditCustomer()
+        private string customer_selected;
+        public EditCustomer(string customerSelected)
         {
             InitializeComponent();
-
+            this.customer_selected = customerSelected;
         }
         private void btnCancel_Click(object sender, EventArgs e)
         {
@@ -25,11 +28,26 @@ namespace WashablesSystem
         private void EditCustomer_Load(object sender, EventArgs e)
         {
             //call displayselectedinfo method here
+            CustomerClass customerClass = new CustomerClass();
+            DataTable customerInfo = new DataTable();
+            customerInfo = customerClass.displaySelectedCustomer(customer_selected);
+
+            foreach (DataRow row in customerInfo.Rows)
+            {
+                txtBoxName.Text = row["customer_name"].ToString();
+                txtBoxEmail.Text = row["customer_email"].ToString();
+                txtBoxPhone.Text = row["customer_phone"].ToString();
+                txtBoxAddress.Text = row["customer_address"].ToString();
+
+            }
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
             //call edit method here
+            CustomerClass customerClass = new CustomerClass(txtBoxName.Text, txtBoxEmail.Text, txtBoxPhone.Text, txtBoxAddress.Text);               
+            customerClass.editCustomer(customer_selected);
+            this.Close();
         }
     }
 }
